93bbc6753e8baf4500717580b148604160b33e49
thuanvm04 <vuminhthuan004@gmail.com>
2025-08-12 11:10:14 +0700

    Add gmail_config.py configuration file

--- custom/custom_gmail/models/gmail_config.py
+++ custom/custom_gmail/models/gmail_config.py
@@ -23,5 +23,7 @@ class GmailConfig(models.Model):
             "redirect_uri": "https://silver-carnival-rqr9pv74r5pf95w-8069.app.github.dev/odoo/gmail/auth/callback",
             "client_id": "934598997197-13d2tluslcltooi7253r1s1rkafj601h.apps.googleusercontent.com",
             "client_secret": "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",
+            
+            
 
         }
