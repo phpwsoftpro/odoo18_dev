2a49786b6b87092956ea89b3e3df920cca09d804
NguyenThuong1109 <thuong721211@gmail.com>
2025-08-14 11:05:41 +0700

    delete custom gmail and gmail marketing folder

--- custom/custom_email_marketing/models/mailing_list_inherit.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class MailingList(models.Model):
-    _inherit = "mailing.list"
-
-    start_date = fields.Date(string="Start Date", default=lambda self: fields.Date.context_today(self))
-
-
